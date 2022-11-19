0f20a067441b9eba043d5bed3ce783c55134f081
pahalce <peaceee.programming@gmail.com>
2022-11-19 18:23:31 +0900

    created header component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import Header from './Header';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
