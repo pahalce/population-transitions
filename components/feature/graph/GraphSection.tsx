1bc527dcb2d1a9b4152ba3a9f28eaec3efb1079c
pahalce <peaceee.programming@gmail.com>
2022-11-20 17:33:42 +0900

    show graph

--- components/feature/graph/GraphSection.tsx
+++ components/feature/graph/GraphSection.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import { useRecoilState } from 'recoil';
+import { DataForPopulationGraphState } from '../../../stores/atoms';
 import SectionTitle from '../../common/SectionTitle';
 import PopulationGraph from './PopulationGraph';
 
 const GraphSection = () => {
+  const [populationData, setPopulationData] = useRecoilState(DataForPopulationGraphState);
   return (
     <div>
       <SectionTitle title="グラフ" />
-      <PopulationGraph data={[]} />
+      <PopulationGraph data={populationData} />
     </div>
   );
 };
--- hooks/usePrefs.ts
+++ hooks/usePrefs.ts
@@ -19,7 +19,7 @@ const usePrefs = () => {
 
   const toggleSelectedPrefs = async (pref: Prefecture) => {
     const foundPref = selectedPrefs.find((selectedPref) => selectedPref.prefCode === pref.prefCode);
-    // add to selected pref list
+    // add the prefecture to the selected pref list & graph
     if (!foundPref) {
       setSelectedPrefs((selectedPrefs) => [...selectedPrefs, pref]);
       const popData = await fetchPopulationByPrefCode(pref.prefCode);
@@ -27,9 +27,12 @@ const usePrefs = () => {
       setPopulationData([...populationData, { pref, valueSeries: popData }]);
       return;
     }
-    // remove from selected list
+    // remove the prefecture from selected list & graph
     setSelectedPrefs(
       selectedPrefs.filter((selectedPref) => selectedPref.prefCode !== pref.prefCode),
+    );
+    setPopulationData(
+      populationData.filter((popData) => popData.pref.prefCode !== foundPref.prefCode),
     );
   };
   return { isLoading, isError, prefs, selectedPrefs, error, toggleSelectedPrefs, populationData };
