b3b86a4c8c1b967fce532c4c1eac79e3c990000f
pahalce <peaceee.programming@gmail.com>
2022-11-20 16:01:13 +0900

    fetch population by prefecture name

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,11 +1,23 @@
 import axios from 'axios';
-import { API_URL_PREF } from '../config';
-import { PrefectureResponse } from '../types';
+import { API_URL_POPULATION, API_URL_PREF } from '../config';
+import { PopulationResponse, Prefecture, PrefectureResponse } from '../types';
 
-export const getAllPrefs = () => {
+export const fetchAllPrefs = () => {
   return axios
     .get(API_URL_PREF, { headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY } })
     .then((res) => {
       return res.data.result as PrefectureResponse;
     });
+};
+
+export const fetchPopulationByPrefCode = (prefCode: Prefecture['prefCode']) => {
+  return axios
+    .get(API_URL_POPULATION, {
+      headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY },
+      params: {
+        prefCode: prefCode,
+        cityCode: '-',
+      },
+    })
+    .then((res) => res.data.result.data[0].data as PopulationResponse);
 };
