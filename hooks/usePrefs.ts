c5b73c041199651f437b294388da992d5d93b788
pahalce <peaceee.programming@gmail.com>
2022-11-21 10:51:41 +0900

    bug-fix: avoid spamming the check button

--- hooks/usePrefs.ts
+++ hooks/usePrefs.ts
@@ -3,10 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchAllPrefs, fetchPopulationByPrefCode } from '../lib/axios';
 import { Prefecture } from '../types';
 import { DataForPopulationGraphState, selectedPrefsState } from '../stores/atoms';
+import { useState } from 'react';
 
 const usePrefs = () => {
   const [selectedPrefs, setSelectedPrefs] = useRecoilState(selectedPrefsState);
   const [populationData, setPopulationData] = useRecoilState(DataForPopulationGraphState);
+  const [isLoadingGraphData, setIsLoadingGraphData] = useState(false);
   const {
     isLoading,
     isError,
@@ -22,18 +24,22 @@ const usePrefs = () => {
     // add the prefecture to the selected pref list & graph
     if (!foundPref) {
       setSelectedPrefs((selectedPrefs) => [...selectedPrefs, pref]);
+      setIsLoadingGraphData(true);
       const popData = await fetchPopulationByPrefCode(pref.prefCode);
-
+      setIsLoadingGraphData(false);
       setPopulationData([...populationData, { pref, valueSeries: popData }]);
-      return;
+    } else {
+      if (isLoadingGraphData) {
+        return;
+      }
+      // remove the prefecture from selected list & graph
+      setSelectedPrefs(
+        selectedPrefs.filter((selectedPref) => selectedPref.prefCode !== pref.prefCode),
+      );
+      setPopulationData(
+        populationData.filter((popData) => popData.pref.prefCode !== foundPref.prefCode),
+      );
     }
-    // remove the prefecture from selected list & graph
-    setSelectedPrefs(
-      selectedPrefs.filter((selectedPref) => selectedPref.prefCode !== pref.prefCode),
-    );
-    setPopulationData(
-      populationData.filter((popData) => popData.pref.prefCode !== foundPref.prefCode),
-    );
   };
   return { isLoading, isError, prefs, selectedPrefs, error, toggleSelectedPrefs, populationData };
 };
